8272e4e0e8cbce659490fa4b4e4bf1963ba85ec8
Łuszyński <e-kfuk@gft.com>
2021-11-18 08:43:17 +0100

    Change package names.

--- src/test/java/kluszynski/example/jpa/exercises/JpaExercisesApplicationTests.java
+++ src/test/java/kluszynski/example/jpa/exercises/JpaExercisesApplicationTests.java
@@ -1,4 +1,4 @@
-package kluszynski.example.jpaexercises;
+package kluszynski.example.jpa.exercises;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
