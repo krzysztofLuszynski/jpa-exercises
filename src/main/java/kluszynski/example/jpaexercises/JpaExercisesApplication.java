870dc41350d0ef492ece0c39d7f4f8d0de012123
Łuszyński <e-kfuk@gft.com>
2021-11-18 07:42:03 +0100

    Initial commit with project structure.

---
+++ src/main/java/kluszynski/example/jpaexercises/JpaExercisesApplication.java
@@ -0,0 +1,13 @@
+package kluszynski.example.jpaexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kluszynski/example/jpaexercises/JpaExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package kluszynski.example.jpaexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
